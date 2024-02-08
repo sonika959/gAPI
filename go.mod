5c4c498af709c2238ff1fb461ac890dc14b7aba1
sonika959 <ssonikassharma752@gmail.com>
2024-02-08 18:41:12 -0500

    first own commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/sonika959/gAPI
+
+go 1.21.6
---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+// Message represent a simple JSON message
+type Message struct {
+	Greeting string `json:"greeting"`
+}
+
+func greetHandler(w http.ResponseWriter, r *http.Request) {
+	message := Message{Greeting: "Hello world"}
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(message)
+}
+func main() {
+	http.HandleFunc("/welcome", greetHandler)
+	http.ListenAndServe(":8011", nil)
+}
